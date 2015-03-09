aeff50d4dd7928955a7e565222a816b9b81faed3
boomlord <boombavtvnn@gmail.com>
2015-03-09 15:18:20 +0700

    Update

--- www/js/index.js
+++ www/js/index.js
@@ -1,51 +1,22 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+var onDeviceReady = function () {
+    //do something with codovar here. for example: show an splashscreen here
 };
 
-app.initialize();
+document.addEventListener('deviceready', this.onDeviceReady, false);
+var bmi;
+$(document).on("pageshow", "#index", function () {
+	$("#btnCalc").on('tap', function (event) {
+		event.preventDefault();
+		var height = $("#height").val();
+		var weight = $("#weight").val();
+		height = height/100;
+		bmi = weight / (height * height);
+		alert("Your BMI is: " + bmi.toPrecision(4));
+	});
+
+	$("#btnClear").on('tap', function (event) {
+		event.preventDefault();
+		$("#height").val("");
+		$("#weight").val("");
+	});
+});
